94b1e6198d53f7f96c15d730c71121e71156199a
Jesse Ilc <jesseilc123@gmail.com>
2023-02-10 11:26:35 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(array, string){
+    const win = array.filter(function(name){
+        return name.toLowerCase() === string.toLowerCase()
+    })
+    return win
+}
+function fuzzyMatch(array, string){
+    const sin = array.filter(function(name){
+        return name.toLowerCase().substring(0, string.length) === string.toLowerCase()
+    })
+    return sin
+}
+
+function matchName(array, string){
+    const min = array.filter(function(name){
+        return name.name === string
+    })
+    return min
+}
